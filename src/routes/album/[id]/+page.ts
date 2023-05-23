97cd5072a1508a6356af9b2defb026a73b36ab3e
saveliy-kremen <saveliy4kremen@gmail.com>
2023-05-23 14:00:17 +0300

    Card Page

---
+++ src/routes/album/[id]/+page.ts
@@ -0,0 +1,31 @@
+import { fetchRefresh } from '$helpers';
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch, params }) => {
+	const albumRes = await fetchRefresh(fetch, `/api/spotify/albums/${params.id}`);
+
+	if (!albumRes.ok) {
+		throw error(albumRes.status, 'Failed to load album!');
+	}
+
+	const albumJSON: SpotifyApi.SingleAlbumResponse = await albumRes.json();
+
+	let color = null;
+	if (albumJSON.images.length > 0) {
+		const colorRes = await fetch(
+			`/api/average-color?${new URLSearchParams({
+				image: albumJSON.images[0].url
+			}).toString()}`
+		);
+		if (colorRes.ok) {
+			color = (await colorRes.json()).color;
+		}
+	}
+
+	return {
+		album: albumJSON,
+		title: albumJSON.name,
+		color
+	};
+};
